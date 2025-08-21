6bca62105b7c0b94b52061f3277f58ec9756d083
soficracco <scracco@pioix.edu.ar>
2025-08-21 08:30:31 -0300

    hice los componentes y una pagina para probarlos

---
+++ front/src/app/components/Mensaje.js
@@ -0,0 +1,18 @@
+"use client"
+
+import clsx from "clsx";
+import styles from "@/components/Mensaje.module.css"
+
+export default function Mensaje(props) {
+    return (
+        <>
+            <div className={clsx(
+                styles.mensaje,
+                props.tipo === "enviado" ? styles.enviado : styles.recibido
+            )}>
+                <p className={styles.texto}>{props.texto}</p>
+                <span className={styles.hora}>{props.hora}</span>
+            </div>
+        </>
+    )
+}
